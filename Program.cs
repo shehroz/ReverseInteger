db437061565756dcd4ba43c83c7ac5f010038246
shehroz <shehrozkhan91@hotmail.com>
2020-03-03 22:20:37 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x=1234;
+            //int x = 1534236469;
+            int x = -123;
+            Console.WriteLine(ReverseInteger(x));
+            Console.ReadKey();
+        }
+        public static int ReverseInteger(int x)
+        {
+            string xStr = x.ToString();
+            bool IsNegativeNum = false;
+            
+            StringBuilder str = new StringBuilder();
+            for (int i =xStr.Length - 1; i >= 0; i--)
+            {
+                if (xStr[i] != '-')
+                    str.Append(xStr[i]);
+                else
+                    IsNegativeNum = true;
+            }
+            try
+            {
+                var num = checked(Int32.Parse(str.ToString()));
+                if (IsNegativeNum)
+                    return num *= (-1);
+                return num;
+            }
+            catch (Exception)
+            {
+
+                return 0;
+            }
+            
+        }
+    }
+}
